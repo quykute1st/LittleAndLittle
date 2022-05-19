18020099553d9f2e9c7aededef04309386d77f99
Quy <nguyenphuquy3004@gmail.com>
2022-05-19 13:54:37 +0700

    commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/menu.css" />
+    <link rel="stylesheet" href="css/reset.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="header-boder">
+        <div class="header">
+            <img srcset="images/logo_ngang.png" alt="" class="header-logo">
+        </div>
+        <div class="container">
+            <ul class="frame-menu">
+                <li class="menu-item">
+                    <a href="#" class="menu-link"> Trang chủ</a>
+                </li>
+                <li class="menu-item">
+                    <a href="#" class="menu-link"> Sự kiện</a>
+                </li>
+                <li class="menu-item">
+                    <a href="#" class="menu-link"> Liên hệ</a>
+                </li>
+            </ul>
+            <ul class="frame-phone">
+                <li class="frame-icon">
+                    <img srcset="images/icon_phone.png" alt="" class="icon-phone">
+                </li>
+                <li>
+                    <i class="phone" aria-hidden="true"> </i> 0123456789
+                </li>
+            </ul>
+        </div>
+    </div>
+</body>
+
+</html>
